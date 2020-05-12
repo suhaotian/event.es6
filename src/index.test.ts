50bed43164b9ef18bbb6cd1221408e8316c7246d
suhaotian <h4av@live.cn>
2020-05-12 09:07:48 +0800

    add test; fix bugs; bump version 1.2.3;

---
+++ src/index.test.ts
@@ -0,0 +1,35 @@
+import { Event } from './index';
+
+describe('test', () => {
+  const EE = new Event();
+  const onEmitFn = (payload: any) => {
+    expect(payload).not.toBe(undefined);
+  };
+  test('Event on', () => {
+    EE.on('hello', onEmitFn);
+    EE.on('hello', onEmitFn);
+
+    expect(EE.cbs['hello'].length).toBe(2);
+  });
+
+  test('Event off all', () => {
+    EE.off('hello');
+    expect(EE.cbs['hello']).toBe(undefined);
+  });
+
+  test('Event off', () => {
+    EE.on('hello2', onEmitFn);
+    EE.on('hello2', onEmitFn);
+
+    EE.off('hello2', onEmitFn);
+    expect(EE.cbs['hello2'].length).toBe(1);
+    EE.off('hello2', onEmitFn);
+    expect(EE.cbs['hello2'].length).toBe(0);
+  });
+
+  test('Event once', () => {
+    EE.once('hello', onEmitFn);
+    EE.emit('hello', 'test');
+    expect(EE.cbs['hello'].length).toBe(0);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ type Callback = (name: string, cb: (payload?: any) => void) => void;
 type Dispatch = (name: string, payload: any, other?: any[]) => void;
 
 class Event {
-  handleEmit: (
-    name: string,
-    cb: (payload?: any) => void,
-    other?: any[]
-  ) => void;
+  onEmit: (name: string, cb: (payload?: any) => void, other?: any[]) => void;
   cbs: any;
   add: Callback;
   subscribe: Callback;
@@ -14,25 +10,24 @@ class Event {
   remove: Callback;
   dispatch: Dispatch;
   fire: Dispatch;
+
   constructor(
-    handleEmit?: (
-      name: string,
-      cb: (payload?: any) => void,
-      other?: any[]
-    ) => void
+    onEmit?: (name: string, cb: (payload?: any) => void, other?: any[]) => void
   ) {
     /* handle emit event */
-    this.handleEmit = handleEmit || function() {};
+    if (onEmit) {
+      this.onEmit = onEmit;
+    }
 
-    this.cbs = {};
+    this.cbs = Object.create(null);
     this.subscribe = this.add = this.on;
     this.unsubscribe = this.remove = this.off;
     this.dispatch = this.fire = this.emit;
   }
 
   once(name: string, cb: (payload?: any) => void) {
-    const fn = () => {
-      cb(...arguments);
+    const fn = (...rest: any) => {
+      cb(...rest);
       this.off(name, fn);
     };
     this.on(name, fn);
@@ -47,25 +42,25 @@ class Event {
   }
 
   off(name: string, cb?: (payload?: any) => void) {
-    if (this.cbs.hasOwnProperty(name) && cb) {
+    if (!cb) {
+      delete this.cbs[name];
+      return;
+    }
+    if (this.cbs[name]) {
       for (let i = 0; i < this.cbs[name].length; i++) {
         if (this.cbs[name][i] === cb) {
           this.cbs[name].splice(i, 1);
-          return;
         }
       }
     }
-
-    delete this.cbs[name];
   }
 
   emit(name: string, payload?: any, ...other: any[]) {
-    if (this.cbs.hasOwnProperty(name)) {
+    if (this.cbs[name]) {
       for (let i = 0; i < this.cbs[name].length; i++) {
-        let cb = this.cbs[name][i];
-        cb(payload, ...other);
+        this.cbs[name][i](payload, ...other);
       }
-      this.handleEmit(name, payload, ...other);
+      if (this.onEmit) this.onEmit(name, payload, ...other);
     }
   }
 }
